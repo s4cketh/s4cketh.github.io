0831c474552d45a1767997ac1cdf20b8cf9fe2a2
yangqiuyi <yangqiuyi@cn.nutstore.net>
2024-12-03 20:19:09 +0800

    feat: Add TOC to article interface

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config'
 import pagefind from 'astro-pagefind'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
 import { rehypeHeadingIds } from '@astrojs/markdown-remark'
 import playformCompress from '@playform/compress'
 import { transformerColorizedBrackets } from '@shikijs/colorized-brackets'
@@ -13,6 +12,8 @@ import rehypeAutoLinkHeadings from 'rehype-autolink-headings'
 import rehypeKatex from 'rehype-katex'
 import rehypeSlug from 'rehype-slug'
 import vue from '@astrojs/vue'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 import config from './src/consts'
 
 // https://astro.build/config
@@ -20,6 +21,14 @@ export default defineConfig({
   site: config.site.url,
   vite: {
     assetsInclude: ['**/*.{zip,jpg,jpeg,png,gif,webp,svg,bmp}'],
+    css: {
+      postcss: {
+        plugins: [
+          tailwindcss,
+          autoprefixer,
+        ],
+      },
+    },
   },
   prefetch: {
     prefetchAll: true,
@@ -46,9 +55,6 @@ export default defineConfig({
   integrations: [
     mdx(),
     sitemap(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
     playformCompress({
       CSS: true,
       HTML: true,
