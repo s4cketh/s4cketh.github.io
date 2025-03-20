170cd28a1dcc66f43c6436b42761774e3879fc99
s4cketh <muchawagner322@gmail.com>
2025-03-20 11:52:05 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://s4cketh.github.io',
 	integrations: [
 		theme({
 			site: {
